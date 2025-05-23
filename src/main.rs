dd2ab8e2fb9e504a184b746d364df7bd35a7ee07
no2ca <taiseiono6@gmail.com>
2025-05-23 22:00:17 +0900

    feat: ライブラリクレートに分割・エラーハンドリングを追加

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
-// 2モジュール以上ネストされている
+extern crate minigrep;
 use std::env;
+use std::process;
+
+use minigrep::Config;
 fn main() {
     // 引数を文字列型を格納するベクトルとして集結させる
     let args: Vec<String> = env::args().collect();
-    // ?はデバッグ成型機
-    println!("{:?}", args);
+    
+    // Configのnewを呼び出してインスタンスを生成
+    let config = Config::new(&args).unwrap_or_else(|err|{
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+    
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
