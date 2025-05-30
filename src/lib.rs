f2835b6e4ab29ae2687a4771c7d02b63e3ec102e
no2ca <taiseiono6@gmail.com>
2025-05-30 19:40:18 +0900

    feat: クエリを含む行を出力する機能を追加

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
+use std::mem;
+
+// 外から使うため pub を使う
 pub struct Config {
     pub query: String,
     pub filename: String,
@@ -26,7 +29,69 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
+}
+
+// search関数の定義
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    let query = query.to_lowercase();
+    for line in contents.lines() {
+        if line.to_string().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+// 大文字小文字を区別しないsearch関数用のテスト
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust", "Trust, me."],
+            search(query, contents)
+        );
+
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,10 @@ fn main() {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
     
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
-
+        // エラーコード1で終了する
         process::exit(1);
     }
 }
